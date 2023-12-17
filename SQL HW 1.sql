b029495945192022cac8b170f3711af0732b7d1e
Lorenzo Resendiz <150934193+LorenResen00@users.noreply.github.com>
2023-12-17 14:27:55 -0800

    Update SQL HW 1.sql
    Updated to complete HW assignment.

--- SQL HW 1.sql
+++ SQL HW 1.sql
@@ -8,26 +8,26 @@ from actor
 where last_name = 'Wahlberg';
 
 -- 2. How many payments were made between $3.99 and $5.99?
--- Answer: 5567 (Query Shown Below)
+-- Answer: 0 (Query Shown Below)
+
 Select *
-from payment;
+from payment
+order by amount desc;
 
 select COUNT(amount)
 from payment
 where amount between 3.99 AND 5.99;
 
---This Shows up as '0' in my count!- Need Help 
-
 -- 3. What film does the store have the most of? (search in inventory)
--- Answer: Film 200 with 9 (Query Shown Below)
+-- Answer: Film 193 with 8 (Query Shown Below)
 
 select *
 from inventory;
 
-select film_id,inventory_id
-from inventory;
-
---Need Help finding!
+SELECT film_id , COUNT(*) AS total_movies
+FROM inventory
+GROUP BY film_id 
+ORDER BY total_movies desc;
 
 -- 4. How many customers have the last name ‘William’?
 -- Answer: 0 (Query Shown Below)
@@ -40,22 +40,38 @@ from customer
 where last_name = 'William';
 
 -- 5. What store employee (get the id) sold the most rentals?
--- Answer: 1-8040 (Query Shown Below)
+-- Answer: 2 - 7,304 (Query Shown Below)
 
---Need Help!
+select *
+from payment; 
+
+select COUNT (rental_id) as number_of_rental_sales, staff_id 
+from payment
+group by staff_id 
+order by COUNT (rental_id) desc;
 
 -- 6. How many different district names are there?
 -- Answer: 378 (Query Shown Below)
 
---Need Help!
+select * 
+from address;
+
+select COUNT(distinct district)
+from address;
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id)
 -- Answer: film_id 508 - 15 actors (Query Shown Below)
 
---Try then see if Need Help
+select *
+from film_actor;
+
+select COUNT(actor_id), film_id
+from film_actor
+group by film_id
+order by COUNT (actor_id) DESC;
 
 -- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
--- Answer: 13 (Query Shown Below)
+-- Answer: 21 (Query Shown Below)
 
 select *
 from customer
@@ -64,16 +80,30 @@ SELECT first_name,last_name,customer_id
 FROM customer
 WHERE last_name LIKE '%es';
 
---Need Check to make sure it's accurate!
-
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 -- with ids between 380 and 430? (use group by and having > 250)
 -- Answer: 3 (Query Shown Below)
 
---Need Help
+select *
+from payment;
 
--- 10. Within the film table, how many rating categories are there? And what rating has the most
--- movies total?
--- Answer: 25 ratings, PG-13 has most (Query Shown Below)
+select COUNT (amount) as number_of_payments
+from payment
+where customer_id between 380 and 430
+group by amount
+having COUNT (amount) > 250;
 
---Need Help
+-- 10. Within the film table, how many rating categories are there? And what rating has the most movies total?
+-- Answer: 5 rating categories, PG-13 has most ratings at 224 (Query Shown Below)
+
+select *
+from film;
+
+SELECT COUNT(DISTINCT rating) AS number_of_rating_categories
+FROM film;
+
+SELECT rating, COUNT(*) AS total_movies
+FROM film
+GROUP BY rating
+ORDER BY total_movies desc;
+
